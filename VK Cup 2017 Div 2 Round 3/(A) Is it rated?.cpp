c20e5f5264d367e540acf788b1c4ee84fee0d062
Vohn Daxen Sobresida <sobresidavohndaxen@gmail.com>
2024-06-28 09:42:21 +0800

    FINAL

--- VK Cup 2017 Div 2 Round 3/(A) Is it rated?.cpp
+++ VK Cup 2017 Div 2 Round 3/(A) Is it rated?.cpp
@@ -1,35 +1,48 @@
 #include <iostream>
+#include <string>
 using namespace std;
  
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+string Word(int a, int b, int c) {
+    string letters = "abcdefghijklmnopqrstuvwxyz";
+    int max_val = max(a, max(b, c));
+    int min_val = min(a, min(b, c));
+    int mid_val = (a + b + c) - (max_val + min_val);
+    return string(1, letters[min_val]) + letters[mid_val] + letters[max_val];
+}
  
+int main() {
     int n;
     cin >> n;
-    int arr1[1000], arr2[1000];
-    bool isRated = false, isUnrated = false;
-    int maxRating = 0;
+    int arr1[n], arr2[n];
+    bool isRated = false;
  
     for (int i = 0; i < n; i++) {
         cin >> arr1[i] >> arr2[i];
         if (arr1[i] != arr2[i]) {
             isRated = true;
         }
-        if (maxRating < arr1[i]) {
-            maxRating = arr1[i];
-            if (i > 0) {
-                isUnrated = true;
-            }
-        }
     }
  
     if (isRated) {
-        cout << "rated\n";
-    } else if (isUnrated) {
-        cout << "unrated\n";
+        cout << "rated" << endl;
+        return 0;
+    }
+ 
+    int max_rating = arr1[0];
+    bool isUnrated = true;
+ 
+    for (int i = 1; i < n; i++) {
+        if (max_rating < arr1[i]) {
+            isUnrated = false;
+            break;
+        }
+        max_rating = arr1[i];
+    }
+ 
+    if (!isUnrated) {
+        cout << "unrated" << endl;
     } else {
-        cout << "maybe\n";
+        cout << "maybe" << endl;
     }
  
     return 0;
