435e0cbd551b18d02d94156e2e638758fa5246e4
Vohn Daxen Sobresida <sobresidavohndaxen@gmail.com>
2024-07-03 22:31:02 +0800

    Problem B

--- Educational Codeforce R-167/B. Substring and Subsequence.cpp
+++ Educational Codeforce R-167/B. Substring and Subsequence.cpp
@@ -1,35 +1,37 @@
 #include <bits/stdc++.h>
 
-template <typename CharT>
-void solve() {
-    std::basic_string<CharT> a, b;
-    std::basic_istream<CharT>& input = std::basic_istream<CharT>::std::cin;
-    input >> a >> b;
-
+template <typename StringType>
+int CalculateMinimumOperations(const StringType& a, const StringType& b) {
     int ans = b.size();
-    for (int i = 0; i < b.size(); i++) {
-        int k = i;
-        for (int j = 0; j < a.size(); j++) {
+    for (size_t i = 0; i < b.size(); i++) {
+        size_t k = i;
+        for (size_t j = 0; j < a.size(); j++) {
             if (k < b.size() && a[j] == b[k]) {
                 k++;
             }
         }
-        ans = std::min(ans, i + int(b.size()) - k);
+        ans = std::min(ans, static_cast<int>(i + b.size() - k));
     }
-    std::basic_ostream<CharT>& output = std::basic_ostream<CharT>::std::cout;
-    output << ans + a.size() << "\n";
+    return ans + a.size();
+}
+
+void solve() {
+    std::string a, b;
+    std::cin >> a >> b;
+    std::cout << CalculateMinimumOperations(a, b) << "\n";
 }
 
 int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-
+    
     int t;
     std::cin >> t;
-
+    
     while (t--) {
-        solve<char>();
+        solve();
     }
+    
     return 0;
 }
